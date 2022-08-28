fc7a2996cd824efe46c3b74a7b03f9269c017e5f
+ <+>
2022-08-28 15:29:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-function isPalindrome(word) {
+function isPalindrome(abba) {
   // Write your algorithm here
+  let reversedWord = abba.split("").reverse().join("");
+
+  if (abba === reversedWord){
+    return true;
+
+  } else {
+    return false;
+  }
 }
+
+
 
 /* 
   Add your pseudocode here
 */
+//reverse the string
+//check if the reversed string is equal to the original string
+//if the return is true then the word is palindrome
+//if return is false then the word is not palindrome
 
 /*
   Add written explanation of your solution here
 */
+//A string is a palindrome if it can be read the same from either backwards or forward
+//so in this test we check if the words abbba,racecar,a,robot and ab are palindromes.
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
